286b53697aff75b5376a7a1557901b0783394d2c
Pithiya-Nilesh <112484355+Pithiya-Nilesh@users.noreply.github.com>
2023-06-01 13:21:30 +0530

    get templete from wati api.py

--- whatsapp_app/whatsapp_app/doctype/api.py
+++ whatsapp_app/whatsapp_app/doctype/api.py
@@ -548,4 +548,37 @@ def send_expo_form_template(name, number, equipment):
         "template_name": template
     }
     response = requests.post(url, json=payload, headers=headers)
+
+@frappe.whitelist(allow_guest=True)
+def get_template():
+    access_token, api_endpoint, name_type, version = whatsapp_keys_details()    
+    url = f"{api_endpoint}/{name_type}/{version}/getMessageTemplates"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": access_token
+        }
+    response = requests.post(url, headers=headers)
+    data = response.json()
+    
+    if "messageTemplates" in data:
+        templates = data["messageTemplates"]
+
+        for template in templates:
+            if template["status"] == "APPROVED":
+                element_name = template["elementName"]
+                body = template["bodyOriginal"]
+
+                existing_template = frappe.get_all(
+                    "Templates",
+                    filters={"template_name": element_name, "sample": body},
+                    fields=["name"]
+                )
+
+                if not existing_template:
+                    new_template = frappe.new_doc("Templates")
+                    new_template.template_name = element_name
+                    new_template.sample = body
+                    new_template.insert(ignore_permissions=True)
+                    frappe.db.commit()
+    
     
