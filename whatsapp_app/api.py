414d1bc7eea4d874ee42b5b36889f19f664efb8c
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2023-10-30 19:29:49 +0530

    fix:repliyed test not store

--- whatsapp_app/api.py
+++ whatsapp_app/api.py
@@ -213,10 +213,10 @@ def template_message_replied():
     response = frappe.form_dict
     doc = frappe.db.get_value("Wati Webhook Template Sent", filters={"whatsapp_id": f'{response["whatsappMessageId"]}'}, fieldname=["name"])
     if doc:
-        frappe.db.set_value("Wati Webhook Template Sent", doc, {'is_replied': 1, 'replied_text': response["text"]})
+        frappe.db.set_value("Wati Webhook Template Sent", doc, {'is_replied': 1, 'replied_text': f'{response["text"]}'})
         frappe.db.commit()
-        if response["text"] == "Yes":
-            equipment_list = frappe.db.get_list("Whatsapp Equipment", {"parent": doc}, pluck="name")
+        # if response["text"] == "Yes":
+        #     equipment_list = frappe.db.get_list("Whatsapp Equipment", {"parent": doc}, pluck="name")
         
 
     # doc = frappe.db.get_value(
